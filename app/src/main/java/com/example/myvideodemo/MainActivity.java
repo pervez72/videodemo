1b1190186ed525b3ce81eff08bb45756366f0a61
pervez72 <85360887+pervez72@users.noreply.github.com>
2021-07-19 10:10:10 +0600

    Initial commit

---
+++ app/src/main/java/com/example/myvideodemo/MainActivity.java
@@ -0,0 +1,31 @@
+aqpackage com.example.myvideodemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        videoView=findViewById(R.id.videoViewId);
+
+        Uri uri=Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.hum_music_feborate);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController=new MediaController(this);
+       videoView.setMediaController(mediaController);
+
+        videoView.start();
+
+    }
+}
